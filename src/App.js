afb49627d1954dcfaeab15cd2c5f835f3149689a
Sheikh Shafi <shafiss01318020612@gmail.com>
2022-03-31 20:28:03 +0600

    quidtipon submit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import { useEffect, useState } from 'react';
 import Phones from './Componet/Phones/Phones';
 
 function App() {
-  const addToCard = (id) => {
-    console.log(id)
-  };
-   
    const [phones , setPhone] = useState([]);
+   const [cart , setCart] = useState([]);
+
+   const addToCard = (name) => {
+     const newName = [...cart, name]
+    setCart(newName);
+  };
+
+  const remove = () => {
+    const Carts = [];
+    setCart(Carts)
+  }
+  
    
    
    useEffect(() => {
      fetch('data.json').then(res => res.json()).then(data => {
        setPhone(data)
       });
-   },[])
+   },[]);
   return (
     <div>
       <h1>Phone list</h1>
@@ -29,14 +37,28 @@ function App() {
           addToCard = {addToCard}   />
           )
       }
+      
       </div>
       <div className='cart'>
         <h1>Carts</h1>
+        {
+          cart.map(phone => <h4>Name: {phone.name}</h4>)
+        }
+        <button onClick={remove}>Remove all</button>
       </div>
+      
+      
+    </div>
+    <div>
+    <h1>How dose javascript work?</h1>
+      <h3>javascript programming language is a scripting language. It read the code form up to down. If any variable decleard in the buttom side and use it for any action in the top, that will throw an error.</h3>
+      <h1>How react usestate works?</h1>
+      <h3>The React useState function thats help access to track state in a function component. In usestate the first is vairiable and second is function... and usestate valu is need to a default value</h3>
     </div>
     </div>
 
   );
 }
+
 
 export default App;
--- src/Componet/Phones/Phones.js
+++ src/Componet/Phones/Phones.js
@@ -3,7 +3,7 @@ import './Phones.css';
 import {  ShoppingCartIcon } from '@heroicons/react/solid';
 
 const Phones = (data ) => {
-    
+    // console.log(data.data)
     const {addToCard} = data;
     const { id, img , name , price} = data.data;
     
@@ -15,7 +15,7 @@ const Phones = (data ) => {
                 <div>
                 <h2>Name = {name}</h2>
                 <h1>Price = {price}</h1>
-                <button className='button' onClick={() => addToCard(name)}>Add to card <ShoppingCartIcon className="shop-icon"/></button>
+                <button className='button' onClick={() => addToCard(data.data)}>Add to card <ShoppingCartIcon className="shop-icon"/></button>
                 </div>
             </div>
         </div>
